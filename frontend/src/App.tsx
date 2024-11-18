0cf4b5e49805911ef601853a2837c7c8ccc09b40
0xDVC <neilohene@gmail.com>
2024-11-18 02:57:43 +0000

    chore(analytics): installed vercel analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import Calendar from './components/Calendar';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <Router>
+      <Analytics />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/timetable/:dept/:year" element={<Calendar />} />
